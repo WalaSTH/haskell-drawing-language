0a484707fcf73d033fd4cfcab485f942c4d13a9f
Iván Renison <85908989+IvanRenison@users.noreply.github.com>
2022-03-25 11:24:35 -0300

    Definición data Dibujo

--- Dibujo.hs
+++ Dibujo.hs
@@ -1,4 +1,24 @@
 module Dibujo where
 
--- Definir el lenguaje (reemplazar `()` con lo que corresponda).
-type Dibujo a = ()
+
+{-
+Gramática de las figuras:
+<Fig> ::= Basica <Bas> | Rotar <Fig> | Espejar <Fig> | Rot45 <Fig>
+    | Apilar <Int> <Int> <Fig> <Fig> 
+    | Juntar <Int> <Int> <Fig> <Fig> 
+    | Encimar <Fig> <Fig>
+-}
+
+
+data Dibujo a =
+    Básica a
+    | Rotar (Dibujo a)
+    | Espejar (Dibujo a)
+    | Rot45 (Dibujo a)
+    | Apilar Int Int (Dibujo a) (Dibujo a)
+    | Juntar Int Int (Dibujo a) (Dibujo a)
+    | Encimar (Dibujo a) (Dibujo a)
+
+
+
+
