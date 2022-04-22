3db527e67e886f7268ccce088f93baf5a3292620
Iván Ariel Renison <ivan.renison@mi.unc.edu.ar>
2022-04-22 08:09:13 -0300

    Eliminados paréntesis innecesarios

--- Código/Dibujos/Manzano.hs
+++ Código/Dibujos/Manzano.hs
@@ -13,34 +13,34 @@ module Dibujos.Manzano (
 import Dibujo (Dibujo(..), r180, r270, (^^^))
 import FloatingPic (FloatingPic, rectan, fShape, vacía)
 import Interp (Conf(..))
-import Graphics.Gloss (Rectangle(Rectangle), white) 
+import Graphics.Gloss (Rectangle(Rectangle), white)
 
 data Forma = Rectangulo | Efe | Blanco
 
 interpForma :: Forma -> FloatingPic
-interpForma Rectangulo = rectan 
-interpForma Efe = fShape 
+interpForma Rectangulo = rectan
+interpForma Efe = fShape
 interpForma Blanco = vacía
 
 -- Escalera al abismo (el comienzo de todo)
 escalera :: Int -> Dibujo Forma
-escalera 1 = (Básica Efe) ^^^ Espejar (Básica Efe) ^^^ (r180 (Básica Efe)) ^^^ Espejar (r180 (Básica Efe))
-escalera n = (Rot45 $ escalera (n-1)) ^^^ escalera (n-1)
+escalera 1 = Básica Efe ^^^ Espejar (Básica Efe) ^^^ r180 (Básica Efe) ^^^ Espejar (r180 (Básica Efe))
+escalera n = Rot45 (escalera (n-1)) ^^^ escalera (n-1)
 
 -- Toro
 toro :: Int -> Dibujo Forma
-toro n = (escalera n) ^^^ (Espejar (escalera n)) 
+toro n = escalera n ^^^ Espejar (escalera n)
 
 torosEnfrentados :: Int -> Dibujo Forma
-torosEnfrentados n = (escalera n) ^^^ (Espejar (escalera n)) ^^^ (r180 $ escalera n)  ^^^ (Espejar $ r180 $ escalera n)
+torosEnfrentados n = escalera n ^^^ Espejar (escalera n) ^^^ r180 (escalera n)  ^^^ Espejar (r180 $ escalera n)
 
 
 toroArena :: Dibujo Forma
-toroArena = torosEnfrentados 10 ^^^ (Rotar $ (torosEnfrentados 10))
+toroArena = torosEnfrentados 10 ^^^ Rotar (torosEnfrentados 10)
 
 -- Manzano
 hojas :: Dibujo Forma
-hojas = r270 (torosEnfrentados 5 ^^^ (Rotar $ (toro 10)))
+hojas = r270 (torosEnfrentados 5 ^^^ Rotar (toro 10))
 
 tronco :: Dibujo Forma
 tronco = Básica Rectangulo
@@ -52,10 +52,10 @@ noneto' ::
 noneto'
     p q r
     s t u
-    v w x = 
+    v w x =
     Apilar 2 1 (Juntar 2 1 p (Juntar 1 1 q r)) $
         Apilar 1 1 (Juntar 2 1 s (Juntar 1 1 t u)) $
-            Juntar 2 1 v (Juntar 1 1 w x)  
+            Juntar 2 1 v (Juntar 1 1 w x)
 
 manzano :: Dibujo Forma
 manzano = noneto' hojas    hojas          hojas
@@ -67,7 +67,7 @@ manzanoConfig :: Float -> Float -> Conf Forma
 manzanoConfig x y = Dis {
     name = "Manzano",
     basic = interpForma,
-    fig = manzano, 
+    fig = manzano,
     width = x,
     height = y,
     col = white
@@ -77,7 +77,7 @@ toroConfig :: Float -> Float -> Conf Forma
 toroConfig x y = Dis {
     name = "Toro",
     basic = interpForma,
-    fig = toro 10, 
+    fig = toro 10,
     width = x,
     height = y,
     col = white
@@ -87,7 +87,7 @@ torosEnfrentadosConfig :: Float -> Float -> Conf Forma
 torosEnfrentadosConfig x y = Dis {
     name = "TorosEnfrentados",
     basic = interpForma,
-    fig = torosEnfrentados 10, 
+    fig = torosEnfrentados 10,
     width = x,
     height = y,
     col = white
@@ -97,17 +97,17 @@ toroArenaConfig :: Float -> Float -> Conf Forma
 toroArenaConfig x y = Dis {
     name = "ToroArena",
     basic = interpForma,
-    fig = toroArena, 
+    fig = toroArena,
     width = x,
     height = y,
     col = white
-}  
+}
 
 escaleraConfig :: Float -> Float -> Conf Forma
 escaleraConfig x y = Dis {
     name = "Escalera",
     basic = interpForma,
-    fig = escalera 10, 
+    fig = escalera 10,
     width = x,
     height = y,
     col = white
