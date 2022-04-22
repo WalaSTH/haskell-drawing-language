65c2b80acd7a17b22f8ed657562b34c9ae133406
leo10t <leo.torres@mi.unc.edu.ar>
2022-04-22 03:42:58 -0300

    Se agrega dibujo manzano

--- Código/Dibujos/Manzano.hs
+++ Código/Dibujos/Manzano.hs
@@ -24,12 +24,48 @@ toro n = (escalera n) ^^^ (Espejar (escalera n))
 torosEnfrentados :: Int -> Dibujo Forma
 torosEnfrentados n = (escalera n) ^^^ (Espejar (escalera n)) ^^^ (r180 $ escalera n)  ^^^ (Espejar $ r180 $ escalera n)
 
+
+toroArena :: Dibujo Forma
+toroArena = torosEnfrentados 10 ^^^ (Rotar $ (torosEnfrentados 10))
+
+
+hojas :: Dibujo Forma
+hojas = r270 (torosEnfrentados 5 ^^^ (Rotar $ (toro 10)))
+
+tronco :: Dibujo Forma
+tronco = Básica Rectangulo
+
+noneto' ::
+    Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a
+    -> Dibujo a -> Dibujo a -> Dibujo a -> Dibujo a
+    -> Dibujo a
+noneto'
+    p q r
+    s t u
+    v w x = 
+    Apilar 2 1 (Juntar 2 1 p (Juntar 1 1 q r)) $
+        Apilar 1 1 (Juntar 2 1 s (Juntar 1 1 t u)) $
+            Juntar 2 1 v (Juntar 1 1 w x)  
+-- noneto'
+--     p q r
+--     s t u
+--     v w x = 
+--     Apilar 1 1(Juntar 1 1 p (Juntar 1 1 q r)) $
+--         Apilar 1 1 (Juntar 1 1 s (Juntar 1 1 t u)) $
+--             Juntar 1 1 v (Juntar 1 1 w x)
+
+manzano :: Dibujo Forma
+manzano = noneto' hojas    hojas          hojas
+                (Rotar hojas)    tronco         (r270 hojas)
+               (Básica Blanco) tronco (Básica Blanco)
+
 formaConfig :: Float -> Float -> Conf Forma
 formaConfig x y = Dis {
     name = "L",
     basic = interpForma,
-    fig = torosEnfrentados 10, 
+    fig = escalera 10, 
     width = x,
     height = y,
     col = white
 }
+
